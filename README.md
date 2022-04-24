2c3f57835ec25f837bdb0bd92b1ecf5661c12103
foltone <77021937+foltone@users.noreply.github.com>
2022-04-24 20:52:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# foltone_ascenseur
+
+## Description
+Voici un script d'ascenseur, RageUI et 0.00ms.
+
+### Discord
+https://discord.com/invite/X9ReemrhKh
---
+++ client/cl_ascenseur.lua
@@ -0,0 +1,49 @@
+ESX = nil
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(500)
+	end
+end)
+
+local MenuAscenseur = RageUI.CreateMenu("Ascenseur", 'menu ascenseur');
+
+function RageUI.PoolMenus:Ascenseur()
+    MenuAscenseur:IsVisible(function(Items)
+        for i = 1, #ConfigAscenseur do
+        local v = ConfigAscenseur[i]
+            Items:AddButton(v.Nom, nil, {RightLabel = ">", IsDisabled = false }, function(onSelected)
+                if (onSelected) then
+                    SetEntityCoords(GetPlayerPed(-1), v.position.x, v.position.y, v.position.z)
+                end
+            end)
+        end
+    end, function(Panels)
+    end)
+end
+
+Citizen.CreateThread(function()
+	while true do
+        if ESX.PlayerData.job and ESX.PlayerData.job.name == 'ambulance' then
+            wait = 500
+            local playerCoords = GetEntityCoords(PlayerPedId())
+            for i = 1, #ConfigAscenseur do
+                local v = ConfigAscenseur[i]
+                local distancevestiaire = GetDistanceBetweenCoords(playerCoords, v.position.x, v.position.y, v.position.z, true)
+                if distancevestiaire <= 5.0 then
+                    wait = 0
+                    DrawMarker(6, v.position.x, v.position.y, v.position.z-1, 0.0, 0.0, 9.0, 0.0, 0.0, 0.0, 0.5, 1.0, 0.5, 114, 204, 114, 250, false, false, 2, false, false, false, false)
+                end
+                if distancevestiaire <= 1.0 then
+                    wait = 0
+                    ESX.ShowHelpNotification("Appuyer sur ~g~[E]~s~ pour acceder à ~g~l'ascenseur", 1) 
+                    if IsControlJustPressed(1, 51) then
+                        RageUI.Visible(MenuAscenseur, not RageUI.Visible(MenuAscenseur))
+                    end
+                end
+            end
+        end
+        Citizen.Wait(wait)
+	end
+end)
